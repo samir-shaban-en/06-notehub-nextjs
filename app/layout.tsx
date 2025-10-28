c3de1ce3909f330f517ccfeb9d6dcd45dccbd84f
Samir Shaban <samirshabanpsn@gmail.com>
2025-10-28 16:52:30 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,13 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}></body>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <TanStackProvider>
+          <Header />
+          {children}
+          <Footer />
+        </TanStackProvider>
+      </body>
     </html>
   );
 }
